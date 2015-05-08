0441e1b143ad06c65055709e73254be92b1884c3
Behnam Montazeri <behnamm@stanford.edu>
2015-05-07 17:33:59 -0700

    Added new plots

--- RpcTransportDesign/OMNeT++Simulation/analysis/PlotToolbox.r
+++ RpcTransportDesign/OMNeT++Simulation/analysis/PlotToolbox.r
@@ -3,8 +3,8 @@
 # This scripts accepts command line arguments as below. All the command line
 # arguments are mandatory
 #
-# ./PlotToolbox.r [PLOT_TYPE] [PLOT_TITLE] [FILE_PATH] [FILE_NAME_1] [LOAD_FACTOR_1] ... 
-#           [FILE_NAME_N] [LOAD_FACTOR_N]
+# ./PlotToolbox.r [PLOT_TYPE] [PLOT_TITLE] [FILE_PATH] [FILE_NAME_1]
+#        [LOAD_FACTOR_1] ... [FILE_NAME_N] [LOAD_FACTOR_N]
 # The input file format are csv files that scavetool generates.
 
 library(reshape2)
@@ -38,8 +38,8 @@ for (i in seq(4, length(args), 2)) {
     commentChar <- substring(firstLine$commentLine,1,1)
 
     rawDataList[[length(rawDataList) + 1]] <- 
-        read.table(fileName, comment.char=commentChar, sep=",", colClasses='numeric',
-            col.names = c('time', 'value'))
+        read.table(fileName, comment.char=commentChar, sep=",",
+                colClasses='numeric', col.names = c('time', 'value'))
 
     rawDataList[[length(rawDataList)]]$loadFactor <- 
             rep(loadFactor, length(rawDataList[[length(rawDataList)]]$value))
@@ -68,16 +68,23 @@ if (identical(plotType, "cdf")) {
 
             )
     
-    summary(cdfDataFrame)
+    write.table(cdfDataFrame, file=paste(plotTitle,".","txt",sep="")
+            , sep="    ")
     cdfPlot <- ggplot(cdfDataFrame, aes(x=value, y=CDF))
     pdf(paste(plotTitle,".","pdf",sep=""), width=20, height=20)
 
-    print(cdfPlot + facet_wrap(~loadFactor, nrow=3, scales="free_y")+
-            geom_line(aes(color=loadFactor), size = 2, alpha = 1/2) + 
+    print(cdfPlot + facet_wrap(~loadFactor, nrow=3, scales="free")+
+            geom_step(aes(color=loadFactor), size = 2, alpha = 1/2) + 
             labs(title = plotTitle) + 
-            scale_y_continuous(limit = c(0, 1)) + 
+            scale_y_continuous(limit = c(0, 1)) +  
             theme(axis.text=element_text(size=20),
-            axis.title=element_text(size=20, face="bold")))
+            axis.title=element_text(size=20, face="bold"),
+            panel.grid.major = element_line(size = 0.75, linetype = 'solid',
+                                colour = "black"), 
+            panel.grid.minor = element_line(size = 0.5, linetype = 'solid',
+                                colour = "gray")
+            
+            ))
 
     dev.off()
 }
