f6d9fa5bcf7ba01dba948d8559f02d497199e19e
Behnam Montazeri <behnamm@stanford.edu>
2015-11-24 15:30:26 -0800

    Improve PlotScripts to produce higher quality plot
    Zero based plots and better plot breaks for StretchVsUnsched plot.

--- RpcTransportDesign/OMNeT++Simulation/analysis/PlotScripts/PlotStretchVsUnsched.r
+++ RpcTransportDesign/OMNeT++Simulation/analysis/PlotScripts/PlotStretchVsUnsched.r
@@ -14,21 +14,30 @@ tailStretchVsUnsched <- subset(stretchVsUnsched, !is.na(TailStretch), select=c('
 nCols = length(unique(stretchVsUnsched$WorkLoad))
 pdf("MeanStretchVsUnsched.pdf", width=60, height=90)
 ggplot(avgStretchVsUnsched, aes(x=UnschedBytes, y=MeanStretch)) + 
-        geom_line(aes(color=LoadFactor, size = 3, alpha = 0.8)) +
-        facet_wrap(MsgSizeRange~WorkLoad, scales="free_y", ncol=nCols) +
-        theme(text = element_text(size=30), axis.text.x = element_text(angle=45, vjust=0.5))
+    geom_line(aes(color=LoadFactor, size = 3, alpha = 0.8)) +
+    facet_wrap(MsgSizeRange~WorkLoad, scales="free_y", ncol=nCols) +
+    theme(text = element_text(size=30), axis.text.x = element_text(angle=45, vjust=0.5)) +
+    scale_x_continuous(breaks = unique(stretchVsUnsched$UnschedBytes)) +
+    scale_y_continuous(limits = c(0, NA)) +
+    aes(ymin=0, xmin=0)
 dev.off()
 
 pdf("MedianStretchVsUnsched.pdf", width=60, height=90)
 ggplot(medianStretchVsUnsched, aes(x=UnschedBytes, y=MedianStretch)) + 
-        geom_line(aes(color=LoadFactor, size = 3, alpha = 0.8)) +
-        facet_wrap(MsgSizeRange~WorkLoad, scales="free_y", ncol=nCols) +
-        theme(text = element_text(size=30), axis.text.x = element_text(angle=45, vjust=0.5))
+    geom_line(aes(color=LoadFactor, size = 3, alpha = 0.8)) +
+    facet_wrap(MsgSizeRange~WorkLoad, scales="free_y", ncol=nCols) +
+    theme(text = element_text(size=30), axis.text.x = element_text(angle=45, vjust=0.5)) +
+    scale_x_continuous(breaks = unique(stretchVsUnsched$UnschedBytes)) +
+    scale_y_continuous(limits = c(0, NA)) +
+    aes(ymin=0, xmin=0)
 dev.off()
 
 pdf("TailStretchVsUnsched.pdf", width=60, height=90)
 ggplot(tailStretchVsUnsched, aes(x=UnschedBytes, y=TailStretch)) + 
-        geom_line(aes(color=LoadFactor, size = 3, alpha = 0.8)) +
-        facet_wrap(MsgSizeRange~WorkLoad, scales="free_y", ncol=nCols) +
-        theme(text = element_text(size=30), axis.text.x = element_text(angle=45, vjust=0.5))
+    geom_line(aes(color=LoadFactor, size = 3, alpha = 0.8)) +
+    facet_wrap(MsgSizeRange~WorkLoad, scales="free_y", ncol=nCols) +
+    theme(text = element_text(size=30), axis.text.x = element_text(angle=45, vjust=0.5)) + 
+    scale_x_continuous(breaks = unique(stretchVsUnsched$UnschedBytes)) +
+    scale_y_continuous(limits = c(0, NA)) +
+    aes(ymin=0, xmin=0)
 dev.off()
