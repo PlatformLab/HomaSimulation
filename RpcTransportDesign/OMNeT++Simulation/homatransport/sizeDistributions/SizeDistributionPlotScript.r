5445b47cfacef3cb194808039e7d0d5e947f67c0
Behnam Montazeri <behnamm@stanford.edu>
2015-12-02 12:18:21 -0800

    Added Cumulative Bytes Dist. to workload plot

--- RpcTransportDesign/OMNeT++Simulation/homatransport/sizeDistributions/SizeDistributionPlotScript.r
+++ RpcTransportDesign/OMNeT++Simulation/homatransport/sizeDistributions/SizeDistributionPlotScript.r
@@ -4,6 +4,7 @@
 # "Workload Analysis of a Large-Scale Key-Value Store"
 
 library(ggplot2)
+library(gridExtra)
 predefProb <- read.table("FacebookKeyValueMsgSizeDist.txt", col.names=c('msgSize', 'CDF'), skip=1)
 x<-10^(seq(log10(tail(predefProb$msgSize, 1) + 1), log10(1000000), length.out=100))
 sigma <- 214.476
@@ -13,38 +14,57 @@ x0 <- 15
 y<-(1-(1+k*(x-x0)/sigma)^(-1/k))*(1-y0)+y0
 value <- c(predefProb$msgSize,x)
 cumProb <- c(predefProb$CDF, y)
-cdfFrame <- data.frame(MessageSize=value, CDF=cumProb,
+prob = cumProb-c(0,cumProb[1:length(cumProb)-1])
+byteFrac = prob*value
+cumBytes <- cumsum(byteFrac)/sum(byteFrac)
+cdfFrame <- data.frame(MessageSize=value, CDF=cumProb, CBF=cumBytes,
         WorkLoad="Facebook KeyValue-Servers")
 
 predefProb <- read.table("DCTCP_MsgSizeDist.txt", col.names=c('MessageSize', 'CDF'), skip=1) 
 predefProb$MessageSize <-  predefProb$MessageSize*1500
 predefProb <- rbind(data.frame(MessageSize=c(0,predefProb$MessageSize[1]-1), CDF=c(0,0)), predefProb)
 predefProb$WorkLoad <- "Web Search"
+prob = predefProb$CDF-c(0,predefProb$CDF[1:length(predefProb$CDF)-1])
+byteFrac = prob * predefProb$MessageSize
+cumBytes <- cumsum(byteFrac)/sum(byteFrac)
+predefProb$CBF <- cumBytes
 cdfFrame <- rbind(predefProb, cdfFrame)
 
 predefProb <- read.table("Facebook_CacheFollowerDist_IntraCluster.txt",
         col.names=c('MessageSize', 'CDF'), skip=1) 
 predefProb <- rbind(data.frame(MessageSize=c(0,predefProb$MessageSize[1]-1), CDF=c(0,0)), predefProb)
 predefProb$WorkLoad <- "Facebook Cache-Servers"
+prob = predefProb$CDF-c(0,predefProb$CDF[1:length(predefProb$CDF)-1])
+byteFrac = prob * predefProb$MessageSize
+cumBytes <- cumsum(byteFrac)/sum(byteFrac)
+predefProb$CBF <- cumBytes
 cdfFrame <- rbind(predefProb, cdfFrame)
 
 predefProb <- read.table("Facebook_WebServerDist_IntraCluster.txt",
         col.names=c('MessageSize', 'CDF'), skip=1) 
 predefProb <- rbind(data.frame(MessageSize=c(0,predefProb$MessageSize[1]-1), CDF=c(0,0)), predefProb)
 predefProb$WorkLoad <- "Facebook Web-Servers"
+prob = predefProb$CDF-c(0,predefProb$CDF[1:length(predefProb$CDF)-1])
+byteFrac = prob * predefProb$MessageSize
+cumBytes <- cumsum(byteFrac)/sum(byteFrac)
+predefProb$CBF <- cumBytes
 cdfFrame <- rbind(predefProb, cdfFrame)
 
 predefProb <- read.table("Facebook_HadoopDist_All.txt",
         col.names=c('MessageSize', 'CDF'), skip=1) 
 predefProb <- rbind(data.frame(MessageSize=c(0,predefProb$MessageSize[1]-1), CDF=c(0,0)), predefProb)
 predefProb$WorkLoad <- "Facebook Hadoop-Servers"
+prob = predefProb$CDF-c(0,predefProb$CDF[1:length(predefProb$CDF)-1])
+byteFrac = prob * predefProb$MessageSize
+cumBytes <- cumsum(byteFrac)/sum(byteFrac)
+predefProb$CBF <- cumBytes
 cdfFrame <- rbind(predefProb, cdfFrame)
 
 cdfFrame$WorkLoad <- factor(cdfFrame$WorkLoad)
 
-png(file='WorkloadCDFs.png', width=2400, height=1000)
-valueCdf <- ggplot(cdfFrame, aes(x=MessageSize,y=CDF))
-valueCdf + geom_line(aes(color=WorkLoad), size = 2, alpha = 1) +
+cdfCbf = list()
+cdfCbf[[1]] <- ggplot(cdfFrame, aes(x=MessageSize,y=CDF)) +
+        geom_line(aes(color=WorkLoad), size = 2, alpha = 1) +
         labs(title = "Message Size Distribution Used In Simulation") +
         theme(axis.text=element_text(size=30),
             axis.title=element_text(size=30, face="bold"),
@@ -59,5 +79,22 @@ valueCdf + geom_line(aes(color=WorkLoad), size = 2, alpha = 1) +
         scale_x_log10("Message Sizes (Bytes)")+
         scale_y_continuous(breaks = round(seq(min(cdfFrame$CDF), max(cdfFrame$CDF), by = 0.1),1))
 
-dev.off()
+cdfCbf[[2]] <- ggplot(cdfFrame, aes(x=MessageSize,y=CBF)) +
+        geom_line(aes(color=WorkLoad), size = 2, alpha = 1) +
+        labs(title = "Message Size Distribution Used In Simulation") +
+        theme(axis.text=element_text(size=30),
+            axis.title=element_text(size=30, face="bold"),
+            panel.grid.major = 
+                element_line(size = 0.75, linetype = 'solid', colour = "black"),
+            panel.grid.minor = 
+                element_line(size = 0.5, linetype = 'solid', colour = "gray"),
+            legend.text=element_text(size=30),
+            legend.title=element_text(size=20, face="bold"),
+            legend.position="top",
+            plot.title = element_text(size=20, face="bold")) +
+        scale_x_log10("Message Sizes (Bytes)")+
+        scale_y_continuous(breaks = round(seq(min(cdfFrame$CBF), max(cdfFrame$CBF), by = 0.1),1))
 
+png(file='WorkloadCDFsCBFs.png', width=2400, height=2000)
+do.call(grid.arrange, cdfCbf)
+dev.off()
