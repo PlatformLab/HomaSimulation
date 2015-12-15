45c7bbf096d43120ac5f997592560b59e8491cab
Behnam Montazeri <behnamm@stanford.edu>
2015-12-15 13:44:04 -0800

    A more generalized implementation fo cdfFabricator
    In new implemetation, the cdfFabricator can be used to easily create
    cdf of any workload with "Piecewise LogLinear CBF"

--- RpcTransportDesign/OMNeT++Simulation/homatransport/sizeDistributions/cdfFabricator.r
+++ RpcTransportDesign/OMNeT++Simulation/homatransport/sizeDistributions/cdfFabricator.r
@@ -1,35 +1,39 @@
 #!/usr/bin/Rscript
 
 # This scripts fabricates a cdf from a
-# "Piecewise LogLinear Cumulative Byte Function" of heavy middle workload. 
+# "Piecewise LogLinear Cumulative Byte Function" of a workload.
+# all you need to do is to specify sizeIntervals and cbf (cumulative byte
+# function) values in below. The assumption is that cbf is piecewise linear
+# function of log10(sizeIntervals) with breakpoints defined in sizeIntervals
+# and cbf vectors.
 
-avgSize <- log(10)/(.05*(1/50-1/1000)/(3-log10(50)) + .15*(1/1000-1/7200)/(log10(7200)-3) + .7*(1/7200-1/(10^4.8))/(4.8-log10(7200)) + .1*(1/(10^4.8)-1/(10^5.5))/(5.5-4.8))
-cdfInv <- function(cdf) {
-    ret <- cdf*log(10)/avgSize
-    if (cdf < avgSize*(.05*(1/50-1/1000)/(3-log10(50)))/log(10)) {
-        ret <- ret * (3-log10(50)) / 0.05
-        ret <- 1 / (1/50 - ret)
-    } else if (cdf < avgSize*(.05*(1/50-1/1000)/(3-log10(50)) + .15*(1/1000-1/7200)/(log10(7200)-3))/log(10)) {
-        ret <- ret - .05*(1/50-1/1000)/(3-log10(50))
-        ret <- ret * (log10(7200)-3) / 0.15
-        ret <- 1 / (1/1000 - ret)
-    } else if (cdf < avgSize*(.05*(1/50-1/1000)/(3-log10(50)) + .15*(1/1000-1/7200)/(log10(7200)-3) + .7*(1/7200-1/(10^4.8))/(4.8-log10(7200)))/log(10)) {
-        ret <- ret - .05*(1/50-1/1000)/(3-log10(50)) - .15*(1/1000-1/7200)/(log10(7200)-3)
-        ret <- ret * (4.8-log10(7200)) / 0.7
-        ret <- 1 / (1/7200 - ret)
-    } else {
-        ret <- ret - .05*(1/50-1/1000)/(3-log10(50)) - .15*(1/1000-1/7200)/(log10(7200)-3) - .7*(1/7200-1/(10^4.8))/(4.8-log10(7200))
-        ret <- ret * (5.5-4.8) / 0.1
-        ret <- 1 / (1/(10^4.8) - ret)
-    }
-    return(ret)
-}
+# monotonically increasing vector of msg sizes
+sizeIntervals <- c(50, 1000, 7200, 10^4.8, 10^5.5) 
 
-sizes <- c()
+# monotonically increaing cbf (cumulative byte function) vector. First element
+# is always 0 and last element is always one. Each cbf value in this vector
+# corresponds to a size in the sizeIntervals vector.
+cbf <- c(0, 0.05, 0.2, 0.9, 1)
+
+multipLiers <- (cbf[2:length(cbf)] - cbf[1:length(cbf)-1]) *
+    (1/sizeIntervals[1:length(cbf)-1] - 1/sizeIntervals[2:length(cbf)]) /
+    (log10(sizeIntervals[2:length(cbf)]) - log10(sizeIntervals[1:length(cbf)-1]))
+avgSize <- log(10)/sum(multipLiers)
+cdfRanges <- cumsum(multipLiers)/sum(multipLiers)
+cdfConsts <- c(0, cdfRanges[1:length(cdfRanges)-1])
+
+#create cdf sample vector and find the corresponding sizes.
+#cdf sample vector must be sorted 
 cdf <- seq(0.001, 1, 0.001)
-for (i in cdf)
+sizes <- c()
+for (cdfVal in cdf)
 {
-    sizes <- c(sizes, cdfInv(i))
+    ind <- match(1, findInterval(cdfRanges, cdfVal))
+    invMsgSize <- (1 / sizeIntervals[ind]) - 
+        ((cdfVal - cdfConsts[ind]) * log(10) *
+        (log10(sizeIntervals[ind+1]) - log10(sizeIntervals[ind])) /
+        (cbf[ind+1]-cbf[ind]) / avgSize)
+    sizes <- c(sizes, 1/invMsgSize) 
 }
 
 # round the sizes to integers and remove possible duplicates that rounding might
