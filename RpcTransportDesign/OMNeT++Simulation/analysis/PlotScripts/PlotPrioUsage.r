d6e7f1c790500e65be8956d056d70a1c7f6c7ac6
Behnam Montazeri <behnamm@stanford.edu>
2016-12-31 10:14:21 -0800

    Adds scripts for plotting priority usages

---
+++ RpcTransportDesign/OMNeT++Simulation/analysis/PlotScripts/PlotPrioUsage.r
@@ -0,0 +1,38 @@
+#!/usr/bin/Rscript
+library(reshape2)
+library(ggplot2)
+library(gridExtra)
+library(plyr)
+
+data <- read.table("prioUsage.txt", na.strings = "NA",
+    col.names=c('prioLevels', 'adaptivePrioLevels', 'workload', 'redundancyFactor',
+    'loadFactor', 'nominalLoadFactor', 'priority', 'prioUsagePct_activelyRecv',
+    'prioUsagePct_activelyRecvSched'), header=TRUE)
+data$redundancyFactor <- factor(data$redundancyFactor)
+nominalLoads <- c(50, 60, 70, 80, 90)
+workloads <- c('GOOGLE_SEARCH_RPC', 'FABRICATED_HEAVY_MIDDLE', 'GOOGLE_ALL_RPC','FACEBOOK_HADOOP_ALL')
+prioUsagePct <- subset(data, nominalLoadFactor %in% nominalLoads)
+i <- 0
+plotList <- list()
+textSize = 10 
+yLab = 'Scheduled Priority Time Usage %'
+xLab = 'Priority Level'
+for (wl in workloads) {
+    for (nominalLoad in sort(unique(prioUsagePct$nominalLoadFactor))) {
+        i <- i+1
+        plotTitle = sprintf("Workload: %s, LoadFactor= %f", wl, nominalLoad)
+        tmp = subset(prioUsagePct, workload==wl & nominalLoadFactor==nominalLoad)
+        plotList[[i]] <- ggplot(tmp, aes(x=priority, y=prioUsagePct_activelyRecvSched)) +
+            geom_bar(stat="identity") +
+            theme(text = element_text(size=textSize, face="bold"), legend.position="top",
+                strip.text.x = element_text(size = textSize), strip.text.y = element_text(size = textSize),
+                plot.title = element_text(size = textSize)) +
+            coord_cartesian(ylim=c(0,100)) +
+            labs(title = plotTitle, x = xLab, y = yLab)
+    }
+}
+
+pdf("plots/prioUsage.pdf", width=23, height=15)
+args.list <- c(plotList, list(ncol=length(nominalLoads)))
+do.call(grid.arrange, args.list)
+dev.off()
---
+++ RpcTransportDesign/OMNeT++Simulation/analysis/PlotScripts/procResultFiles.py
@@ -0,0 +1,162 @@
+#!/usr/bin/python
+
+from numpy import *
+from glob import glob
+from optparse import OptionParser
+from pprint import pprint
+from functools import partial
+from xml.dom import minidom
+import math
+import os
+import subprocess
+import random
+import re
+import sys
+import warnings
+sys.path.insert(0, os.environ['HOME'] +\
+    '/Research/RpcTransportDesign/OMNeT++Simulation/analysis')
+from parseResultFiles import *
+from MetricsDashBoard import *
+
+if __name__ == '__main__':
+    parser = OptionParser(description='This scripts takes in a fileList file'
+        ' name argument containing homa\'s scalar result files filenames, one'
+        ' per each line. Computes the wasted bandwidth and/or priority usages'
+        ' from all of those result files and dump results in outputFile'
+        ' argument. The output file format is tabulated similar to R dataframe'
+        ' format.')
+
+    parser.add_option('--fileList', metavar='FILENAME', default = '',
+        dest='fileList', help='name of file, containing scalar result'
+        ' files name, one item per line')
+    parser.add_option('--outputType', metavar='wp', default = '',
+        dest='outputType', help="Any combination of 'w', 'p'. 'w' for wastedBw,"
+        " 'p' for priority usage percentage.")
+
+    options, args = parser.parse_args()
+    fileList = options.fileList
+    if fileList == '':
+        sys.exit('argument --fileList is required!')
+
+    outputType = AttrDict({'wastedBw':False, 'prioUsage':False})
+    hasValidType = False
+    for char in options.outputType:
+        if char=='w':
+            outputType.wastedBw = True
+            hasValidType = True
+            wastedBwFile = 'wastedBw.txt'
+        if char=='p':
+            hasValidType = True
+            outputType.prioUsage = True
+            prioUsageFile = 'prioUsage.txt'
+
+    if not(hasValidType):
+        sys.exit('Enter a valid combination for'' --outputType command line'
+        ' option!')
+
+    # open file list and read the result files
+    absPath = os.path.abspath(fileList)
+    absDir = os.path.dirname(absPath)
+    f = open(absPath)
+    resultFiles = [line.rstrip('\n') for line in f]
+    f.close()
+
+    
+    # compute output for result files
+    wastedBw = []
+    prioTimeUsagesPct = []
+    for dirFile in resultFiles:
+        filename = os.path.join(absDir, dirFile)
+        print filename
+        xmlConfigFile = os.environ['HOME'] + '/Research/RpcTransportDesign/'\
+            'OMNeT++Simulation/homatransport/src/dcntopo/config.xml'        
+
+        sp = ScalarParser(filename)
+        parsedStats = AttrDict()
+        parsedStats.hosts = sp.hosts
+        parsedStats.tors = sp.tors
+        parsedStats.aggrs = sp.aggrs
+        parsedStats.cores = sp.cores
+        parsedStats.generalInfo = sp.generalInfo
+        xmlParsedDic = parseXmlFile(xmlConfigFile, parsedStats.generalInfo)
+        trafficDic = computeBytesAndRates(parsedStats, xmlParsedDic)
+        digestTrafficInfo(trafficDic.rxHostsTraffic.nics.rx, 'RX NICs Recv:')
+
+        redundancyFactor = parsedStats.generalInfo['numSendersToKeepGranted']
+        workloadType =  parsedStats.generalInfo['workloadType']
+        nominalLoadFactor = float(parsedStats.generalInfo.rlf)*100
+        loadFactor =  100 *\
+            trafficDic.rxHostsTraffic.nics.rx.trafficDigest.avgRate /\
+            int(parsedStats.generalInfo.nicLinkSpeed.strip('Gbps'))
+      
+        if outputType.prioUsage:
+            prioUsageStatsDigest = []
+            computePrioUsageStats(parsedStats.hosts, parsedStats.generalInfo,
+                xmlParsedDic, prioUsageStatsDigest)
+            adaptiveSchedPrioLevels = int(eval(
+                parsedStats.generalInfo.adaptiveSchedPrioLevels))
+            prioLevels = int(eval(parsedStats.generalInfo.prioLevels))
+            for prioStats in prioUsageStatsDigest:
+                priority = prioStats.priority
+                #record results only for scheduled priorities
+                if priority < prioLevels-adaptiveSchedPrioLevels:
+                    continue
+                usageTimePct = prioStats.usageTimePct
+                prioTimeUsagesPct.append((prioLevels, adaptiveSchedPrioLevels,
+                    workloadType, redundancyFactor, loadFactor,
+                    nominalLoadFactor, priority, usageTimePct.activelyRecv,
+                    usageTimePct.activelyRecvSched))
+
+        if outputType.wastedBw:
+            activeAndWasted = computeWastedTimesAndBw(parsedStats, xmlParsedDic)
+            wastedBw.append((workloadType, redundancyFactor, loadFactor,
+                nominalLoadFactor, activeAndWasted.rx.fracTotalTime,
+                activeAndWasted.rx.fracActiveTime))
+
+    tw_h = 40
+    tw_l = 40
+
+    if outputType.prioUsage:
+        # create output file
+        fdPrioUsage = open(os.environ['HOME'] + "/Research/RpcTransportDesign" +
+            "/OMNeT++Simulation/analysis/PlotScripts/" + prioUsageFile, 'w')
+        fdPrioUsage.write('prioLevels'.center(tw_l) +
+            'adaptivePrioLevels'.center(tw_l) + 'workload'.center(tw_l) +
+            'redundancyFactor'.center(tw_h) + 'loadFactor'.center(tw_l) +
+            'nominalLoadFactor'.center(tw_l) + 'priority'.center(tw_l) +
+            'prioUsagePct_activelyRecv'.center(tw_l) +
+            'prioUsagePct_activelyRecvSched'.center(tw_l) + '\n')
+
+        # dump the results in outputfiles
+        for prioUsage in prioTimeUsagesPct:
+            fdPrioUsage.write(
+                '{0}'.format(prioUsage[0]).center(tw_l) +
+                '{0}'.format(prioUsage[1]).center(tw_h) +
+                '{0}'.format(prioUsage[2]).center(tw_l) +
+                '{0}'.format(prioUsage[3]).center(tw_l) +
+                '{0}'.format(prioUsage[4]).center(tw_l) +
+                '{0}'.format(prioUsage[5]).center(tw_l) +
+                '{0}'.format(prioUsage[6]).center(tw_l) +
+                '{0}'.format(prioUsage[7]).center(tw_l) +
+                '{0}'.format(prioUsage[8]).center(tw_l) + '\n')
+        fdPrioUsage.close()
+
+    if outputType.wastedBw:
+        # create output file
+        fdWasted = open(os.environ['HOME'] + "/Research/RpcTransportDesign" +
+            "/OMNeT++Simulation/analysis/PlotScripts/" + wastedBwFile, 'w')
+        fdWasted.write('workload'.center(tw_l) +
+            'redundancyFactor'.center(tw_h) + 'loadFactor'.center(tw_l) +
+            'nominalLoadFactor'.center(tw_l) + 'wastedBw'.center(tw_l) +
+            'activeWastedBw'.center(tw_l) + '\n')
+        # dump the results in outputfiles
+        for wasteTuple in wastedBw:
+            fdWasted.write(
+                '{0}'.format(wasteTuple[0]).center(tw_l) +
+                '{0}'.format(wasteTuple[1]).center(tw_h) +
+                '{0}'.format(wasteTuple[2]).center(tw_l) +
+                '{0}'.format(wasteTuple[3]).center(tw_l) +
+                '{0}'.format(wasteTuple[4]).center(tw_l) +
+                '{0}'.format(wasteTuple[5]).center(tw_l) + '\n')
+        fdWasted.close()
+
